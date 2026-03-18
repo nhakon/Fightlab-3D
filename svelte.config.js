9e456d02c775158678cd61d235699c5ccacf600f
nhakon <Nordby.Haakon@gmail.com>
2026-03-18 12:33:21 +0100

    Use SvelteKit Vercel adapter for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,10 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 
 const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		// Relax CSP to allow Vite HMR and environments that block eval by default
 		csp: {
