a2b6ad27add65330bd1946f8607d5f3eb830d39c
nhakon <Nordby.Haakon@gmail.com>
2026-03-18 12:51:11 +0100

    Pin SvelteKit Vercel adapter runtime to Node 20

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,9 @@ const dev = process.env.NODE_ENV === 'development';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			runtime: 'nodejs20.x'
+		}),
 		// Relax CSP to allow Vite HMR and environments that block eval by default
 		csp: {
 			mode: 'auto',
