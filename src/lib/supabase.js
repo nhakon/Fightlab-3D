244bd3866594934d73be534f6df3676dd0242277
nhakon <Nordby.Haakon@gmail.com>
2026-03-19 14:18:31 +0100

    Use static public Supabase env

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
-import { env } from '$env/dynamic/public';
+import { PUBLIC_SUPABASE_PUBLISHABLE_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 
-const supabaseUrl = env.PUBLIC_SUPABASE_URL;
-const supabasePublishableKey = env.PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+const supabaseUrl = PUBLIC_SUPABASE_URL;
+const supabasePublishableKey = PUBLIC_SUPABASE_PUBLISHABLE_KEY;
 
 export const isSupabaseConfigured = Boolean(supabaseUrl && supabasePublishableKey);
 
